6fd18a401145872f3522ec64d655f21a52fdd8e4
Komal6627 <komalkilledar1996@gmail.com>
2024-02-22 12:06:26 +0530

    created review  model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,8 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import BLANK_CHOICE_DASH
+from django.utils import timezone
 # Create your models here.
 
 class Product(models.Model):
@@ -14,11 +16,25 @@ class Product(models.Model):
     numReviews =  models.IntegerField(null = True, blank = True, default = 0)
     price = models.DecimalField(max_digits = 12, decimal_places = 2, null = True, blank = True)
     countInStock = models.IntegerField(null = True, blank = True, default = 0)
-    createdAt = models.DateTimeField(auto_now_add = True)
-    _id = models.AutoField(primary_key=True,editable=False)
+    createdAt = models.DateTimeField(default=timezone.now)
+    _id = models.AutoField(primary_key=True,editable=False, default="")
 
     def  __str__(self) -> str:
          return f"{self.name} | {self.brand} | {str(self.price)}"
     
+class Review(models.Model):
+     product = models.ForeignKey(Product, on_delete = models.SET_NULL, null = True)
+     user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
+     name = models.CharField(max_length = 200, null =True, blank = True)
+     rating =  models.IntegerField(null = True, blank = True, default = 0)
+     comment = models.TextField(null = True, blank = True)
+     createdAt = models.DateTimeField(auto_now_add=True)
+     _id = models.AutoField(primary_key=True,editable=False, default="")
+
+     def  __str__(self) -> str:
+         return f"{str(self.rating)}"
+     
+
+     
 
     
