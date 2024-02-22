3952053ef43ac0352d5e7681664ae3eaa0619722
Komal6627 <komalkilledar1996@gmail.com>
2024-02-22 11:23:01 +0530

    created product model

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
-]
+#     path('api/products/',include('api.urls.product_urls')),
+#     path('api/users/',include('api.urls.user_urls')),
+#     path('api/orders/',include('api.urls.order_urls')),
+ ]
+
+
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
