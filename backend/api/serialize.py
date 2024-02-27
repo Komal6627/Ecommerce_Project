fb0cdc7d9ba82bfaa227d3dfeddea3c56e898bc5
Komal6627 <komalkilledar1996@gmail.com>
2024-02-27 18:22:31 +0530

    OrderSerializer is complete

--- backend/api/serialize.py
+++ backend/api/serialize.py
@@ -61,8 +61,29 @@ class OrderItemSerializer(serializers.ModelSerializer):
         model = OrderItem
         fields = '__all__'
 
+class OrderSerializer(serializers.ModelSerializer):
+    orderItem = serializers.SerializerMethodField(read_only = True)
+    ShippingAddress = serializers.SerializerMethodField(read_only = True)
 
+    class Meta:
+        model = Order
+        fields = '__all__'
 
-
+    def get_orderItems(self,obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many = True)
+        return serializer.data
+    
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(obj.shippingaddress,many=False).data
+        except:
+            address = False
+        return address
+    
+    def get_User(self,obj):
+        items = obj.user
+        serializer = UserSerializer(items, many = False)
+        return serializer.data
     
         
