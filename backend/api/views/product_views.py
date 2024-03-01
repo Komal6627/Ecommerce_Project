a8b3ee17c4563272392ff692de72c2368d1c7f48
Komal6627 <komalkilledar1996@gmail.com>
2024-03-01 12:05:25 +0530

    getProducts method completed

---
+++ backend/api/views/product_views.py
@@ -0,0 +1,39 @@
+from django.core import paginator
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from api.models import *
+from api.serializers import ProductSerializer
+
+# GET all the products with query
+@api_view(['GET'])
+def getProducts(request):
+    query = request.query_params.get('keyword')
+    if query is None:
+        query = ''
+    
+    products = Product.objects.filter(name__iscontains = query).order_by('_id')
+    page = request.query_params.get('page')
+    if page is None or page.strip() == '':
+        page = 1
+    else:
+        try:
+            page = int(page)
+        except ValueError:
+            page = 1
+    paginator = Paginator(products, 8)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(paginator.num_pages)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    
+    serializer = ProductSerializer(products, many = True)
+    return Response({'products': serializer.data, 'page': page, 'pages':paginator.num_pages})
